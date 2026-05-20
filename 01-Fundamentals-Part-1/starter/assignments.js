56c29567f16bc24f3f71071898c998953e2030fc
EXT-Zamora, Mario <mario.zamora2@boeing.com>
2026-05-20 11:15:09 -0600

    saving progress

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -52,3 +52,25 @@ if (language === 'English' && population < 50 && !isIsland) {
 } else {
     console.log(`${country} does not meet your criteria :(`);
 }
+
+switch (language) {
+    case 'Mandarin':
+    case 'Chinese':
+        console.log('MOST number of native speakers!');        
+        break;
+    case 'Spanish':
+        console.log('2nd place in number of native speakers');
+        break;
+    case 'English':
+        console.log('3rd place');
+        break;
+    case 'Hindi':
+        console.log('Number 4');
+        break;
+    case 'Arabic':
+        console.log('5th most spoken language');
+        break;
+    default:
+        console.log('Great language too :D');
+}
+
